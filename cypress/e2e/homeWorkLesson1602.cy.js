4b206f27ecb0e1218f530f4d9c68e71f309a136b
Valdemar3 <bearbdsm3@gmail.com>
2023-04-07 14:12:27 +0300

    finish HoweWork

--- cypress/e2e/homeWorkLesson1602.cy.js
+++ cypress/e2e/homeWorkLesson1602.cy.js
@@ -1,11 +1,11 @@
 //importing datas from fixtures
 import * as forHoweWork1602 from '../fixtures/forHoweWork1602.json'
-import { loggedInFunc, searchFunc } from '../support/helperHoweWork1602'
+import { loggedInFunc, searchFunc, findFunc, productFinderWithRecursion, bookProduct } from '../support/helperHoweWork1602'
 
 
 describe ('Test Suite for AutomStore', () => {
  
-    it('LoggedIn', () => {
+    it('LoggedIn, Find, Book of a Good', () => {
 
     //Call LoggedIn Function
     loggedInFunc(forHoweWork1602)
@@ -13,16 +13,14 @@ describe ('Test Suite for AutomStore', () => {
     //Call Searching Function
     searchFunc(forHoweWork1602)
     
-    //Find Name of goods with IF operator
-    cy.log('**Call Finding Function**')
-    cy.get('.prdocutname').should('be.visible')
-        .and('have.attr','title')
-        .and('have.value', forHoweWork1602.nameOfGood)
-    
-    // findFunc()
+    //findFunc(forHoweWork1602) //Problem is that function isnt stopping when I am on the good's page
+
+    //Producfinder function RECURSION
+    productFinderWithRecursion(forHoweWork1602)
     
     //Make order
     cy.log('**Call Order Function**')
+    bookProduct()
     //orderFunc()  
     })
     
--- cypress/support/helperHoweWork1602.js
+++ cypress/support/helperHoweWork1602.js
@@ -12,7 +12,92 @@ export function loggedInFunc(forHoweWork1602) {
 }
 
 export function searchFunc(forHoweWork1602) {
-        //Find searching input and type E and search
-        cy.log('**Find searching input and type E and search **')
-        cy.get('#search_form').type(forHoweWork1602.searchingWord).submit()
+    //Find searching input and type E and search
+    cy.log('**Find searching input and type E and search **')
+    cy.get('#search_form').type(forHoweWork1602.searchingWord).submit()
 }
+
+export function productFinderWithRecursion (forHoweWork1602) {
+    cy.get('body').then((body) => {
+        if (body.find(`[title="${forHoweWork1602.nameOfGood4}"]`).length > 0) {
+            cy.get(`[title="${forHoweWork1602.nameOfGood4}"]`).click()
+            cy.log('**BINGO**')
+        } else {
+            cy.log('**NOT BINGO**')
+            cy.get('.pagination a[href]').contains('>').click()
+            productFinderWithRecursion(forHoweWork1602)
+        }
+    })
+}
+
+export function bookProduct () {
+    cy.get('.productpagecart').click()
+    cy.get('#cart_checkout1').click()
+    cy.get('#checkout_btn').click()
+    //checkin for succces of order
+    cy.get('.maintext').contains('Your Order Has Been Processed!')
+}
+
+//Problem is that function isnt stopping when I am on the good's page
+
+// export const productFinder = (forHoweWork1602) => {
+//     cy.get('.pull-right .pagination').find('li').then(elm => {
+//         //
+//         for (let i = 0; i < elm.length; i++) {
+//             cy.get(".prdocutname").then(($a) => {
+//                 if ($a.text().includes(`${forHoweWork1602.nameOfGood4}`)) {
+//                     cy.get(`[title="${forHoweWork1602.nameOfGood4}"]`).click()
+//                     cy.log('***FOUND***')
+//                 } else {
+//                     cy.get('.mat-paginator-navigation-next')
+//                 }
+//             })
+//         }
+//     })
+// }
+
+//Problem is that function isnt stopping when I am on the good's page
+// export function findFunc(forHoweWork1602) {
+//     //cycle for finding Good
+//     cy.get('.pagination').find('li').then (findGood => {
+        
+//         for (let i=1; i<=findGood.length; i++)
+//             cy.get('.prdocutname').then(mMatch => {
+//                 if (mMatch.text().includes(forHoweWork1602.nameOfGood4) ) {
+//                     cy.get(`[title="${forHoweWork1602.nameOfGood4}"]`).click()
+//                     cy.log('**Bingo**',i)
+                    
+//                 } 
+//                 else { 
+//                     cy.log('**NOT Bingo**',i,findGood.length)
+//                     cy.get('.pagination a[href]').contains('>').click()
+//                         }
+//         })
+//     }
+// )}
+
+//Problem is that function isnt stopping when I am on the good's page
+// export function findFunc(forHoweWork1602) {
+//     //cycle for finding Good
+//     cy.get('.pagination').find('li').then (findGood => {
+//         for (let i=1; i<=findGood.length; i++) {
+//             cy.get('.prdocutname').then(mMatch => {
+//                 if (mMatch.text().includes(forHoweWork1602.nameOfGood4))
+//                 {
+//                     cy.get(`[title="${forHoweWork1602.nameOfGood4}"]`).click()
+//                     cy.log('**Bingo**')
+                    
+//                     //calling buyin function
+        
+//                 }
+//                 else {
+//                     cy.log('**NOT Bingo**')
+//                     cy.get('.pagination a[href]').contains('>').click()
+//                     findFunc(forHoweWork1602)
+//                 }
+//             })
+            
+//         }
+//     })  
+           
+// }
